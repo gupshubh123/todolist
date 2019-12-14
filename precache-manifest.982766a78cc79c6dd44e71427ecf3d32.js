72a8cb1c24852392fbaa638ba4f2a169a04759a9
shubham gupta <shubham.gupta2@aspiringminds.in>
2019-12-15 01:02:41 +0530

    Updates

---
+++ precache-manifest.982766a78cc79c6dd44e71427ecf3d32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5aebb8504759fc06b98686da0f4c4b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a05531159c5e9bd4a0bc",
+    "url": "/todolist/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a52c7bb1305c94cf51c2",
+    "url": "/todolist/static/css/main.4173c4dc.chunk.css"
+  },
+  {
+    "revision": "a05531159c5e9bd4a0bc",
+    "url": "/todolist/static/js/2.6f066404.chunk.js"
+  },
+  {
+    "revision": "18db333adb41a97042cb1281e6b75be4",
+    "url": "/todolist/static/js/2.6f066404.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a52c7bb1305c94cf51c2",
+    "url": "/todolist/static/js/main.8f7b16a6.chunk.js"
+  },
+  {
+    "revision": "b5631cdcdd14849564a7",
+    "url": "/todolist/static/js/runtime-main.75b5d516.js"
+  },
+  {
+    "revision": "080d0fda628d14d4d3f14992c44dad02",
+    "url": "/todolist/static/media/empty_list.080d0fda.png"
+  }
+]);
